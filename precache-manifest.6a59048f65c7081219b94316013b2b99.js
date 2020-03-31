7e3da57dcfa31d23f8a49886c111121bf2f59f5e
Yevhen R <7374618+yevhen-romanenko@users.noreply.github.com>
2020-03-31 19:26:39 +0300

    Updates

---
+++ precache-manifest.6a59048f65c7081219b94316013b2b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4e3cecf19d1b03edccab92777b354d",
+    "url": "/address-book/index.html"
+  },
+  {
+    "revision": "5abfd911218859176dc4",
+    "url": "/address-book/static/css/main.9754ac47.chunk.css"
+  },
+  {
+    "revision": "4d328e98d08a453f5013",
+    "url": "/address-book/static/js/2.2fd202df.chunk.js"
+  },
+  {
+    "revision": "02b3897220fa0ff5376734687e85d84a",
+    "url": "/address-book/static/js/2.2fd202df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5abfd911218859176dc4",
+    "url": "/address-book/static/js/main.d8e4dbee.chunk.js"
+  },
+  {
+    "revision": "3d8c6c542c6b4aff3342",
+    "url": "/address-book/static/js/runtime-main.cc787942.js"
+  }
+]);
